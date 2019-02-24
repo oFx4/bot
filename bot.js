46f89cf5a1f1adf46fe3dd762e12f0cb85d09ce3
oFx4 <kiahohoho@naver.com>
2019-02-24 15:25:01 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-        message.channel.sendMessage('test1');
+        message.channel.sendMessage('');
   	}
 });
 
